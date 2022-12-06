178d3391d3232adfa6f2555cfb2f9ad6864e5d41
sa02045 <sa02045@naver.com>
2022-12-07 00:29:26 +0900

    feat: bottom navigation 추가

--- src/layout/MainLayout.tsx
+++ src/layout/MainLayout.tsx
@@ -3,30 +3,57 @@ import { Outlet } from 'react-router-dom';
 import { BottomNavigation, BottomNavigationAction } from '@mui/material';
 import { AiOutlineHome, AiOutlinePlusSquare } from 'react-icons/ai';
 import { RxPerson } from 'react-icons/rx';
-import { Link } from 'react-router-dom';
+import { Container } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
+import './MainLayout.css';
 
 function Root() {
-  const [currentPage, setCurrentPage] = React.useState();
+  const [navigationValue, setNavigationValue] = React.useState();
+  const navigate = useNavigate();
+
   return (
-    <div>
-      <Outlet />
+    <Container maxWidth="sm" sx={{ padding: 0, height: '100%' }}>
+      <main>
+        <Outlet />
+      </main>
       <BottomNavigation
-        value={currentPage}
+        sx={{
+          backgroundColor: '#17171b',
+          position: 'fixed',
+          padding: 0,
+          bottom: 0,
+          height: '80px',
+          width: '100%',
+          display: 'flex',
+          alignItems: 'center',
+        }}
+        className="bottom-navigation"
+        value={navigationValue}
         onChange={(event, newValue) => {
-          setCurrentPage(newValue);
+          setNavigationValue(newValue);
+          navigate(newValue);
         }}
       >
-        <Link to="/home">
-          <BottomNavigationAction icon={<AiOutlineHome />} />
-        </Link>
-        <Link to="upload">
-          <BottomNavigationAction icon={<AiOutlinePlusSquare />} />
-        </Link>
-        <Link to="profile">
-          <BottomNavigationAction icon={<RxPerson />} />
-        </Link>
+        <BottomNavigationAction
+          value={'home'}
+          sx={{ color: '#fff', fontSize: '36px', height: '100%', padding: 0 }}
+          icon={<AiOutlineHome />}
+          className="bottom-navigation-action"
+        />
+        <BottomNavigationAction
+          value={'upload'}
+          sx={{ color: '#fff', fontSize: '36px', height: '100%', padding: 0 }}
+          icon={<AiOutlinePlusSquare />}
+          className="bottom-navigation-action"
+        />
+        <BottomNavigationAction
+          value={'profile'}
+          sx={{ color: '#fff', fontSize: '36px', height: '100%', padding: 0 }}
+          icon={<RxPerson />}
+          className="bottom-navigation-action"
+        />
       </BottomNavigation>
-    </div>
+    </Container>
   );
 }
 
