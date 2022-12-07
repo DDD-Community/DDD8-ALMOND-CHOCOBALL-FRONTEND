77e0c793ef57111655ed1b526794f289b869cad7
sa02045 <sa02045@naver.com>
2022-12-07 21:37:54 +0900

    style: home 페이지 style

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import Header from '../components/Header';
 import { useNavigate } from 'react-router-dom';
 import { AiOutlineLeft, AiOutlineSearch } from 'react-icons/ai';
+import './Home.css';
+import Tag from '../components/Tag';
 
 function Home() {
   const navigate = useNavigate();
   return (
-    <div>
+    <div className="home-container">
       <Header
         left={
           <AiOutlineLeft
@@ -22,6 +24,26 @@ function Home() {
           />
         }
       />
+
+      <div className="image-container">
+        <img
+          className="main-image"
+          alt="test"
+          src="https://post-phinf.pstatic.net/MjAyMDA3MDZfMjA5/MDAxNTk0MDI1NzQ0ODA0.UjEnc6IODtka_-lyE8pPFF0OD0xqKq12kG9Y1T5KkXgg.MZQfSHOZbYU-hrMAvFJxMs_8PX5SKCR8gaURwMABeYMg.JPEG/5_%281%29.jpg?type=w1200"
+        />
+        <section>
+          <div className="title-container">
+            오늘 데이트 하는데 어떤 스타일이 제일 괜찮나요?
+          </div>
+          <div className="tag-container">
+            <Tag text="태그태그" />
+            <Tag text="태그태그" />
+            <Tag text="태그태그" />
+            <Tag text="태그태그" />
+            <Tag text="태그태그" />
+          </div>
+        </section>
+      </div>
     </div>
   );
 }
