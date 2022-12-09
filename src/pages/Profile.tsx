59dd377b029a535b0536abc07127bb61b81761c7
sa02045 <sa02045@naver.com>
2022-12-10 02:01:25 +0900

    feat: profile image 리스트

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,10 +1,19 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Header from '../components/Header';
 import { useNavigate } from 'react-router-dom';
-import { AiOutlineLeft, AiOutlineSearch } from 'react-icons/ai';
+import { AiOutlineLeft } from 'react-icons/ai';
+import { ImageList, ImageListItem } from '@mui/material';
+import { useState } from 'react';
+import './Profile.css';
+interface ImageItem {
+  img: string;
+  title: string;
+}
 
 function Profile() {
   const navigate = useNavigate();
+  const [imageItem, setImageItem] = useState<ImageItem[]>([]);
+
   return (
     <div>
       <Header
@@ -16,6 +25,34 @@ function Profile() {
         }
         centerText={'마이페이지'}
       />
+
+      <div className="profile-container">
+        <h3 className="profile-id">춤추는 원숭</h3>
+        <span className="profile-posts">게시물 13</span>
+      </div>
+      <ImageList
+        cols={2}
+        style={{
+          position: 'absolute',
+          left: 0,
+          width: '100vw',
+          height: '100vh',
+          overflowY: 'scroll',
+          paddingBottom: '300px',
+        }}
+        gap={1}
+      >
+        {Array.from({ length: 9 }).map((item, index) => (
+          <ImageListItem key={index}>
+            <img
+              src={require('./testImage.png')}
+              srcSet={require('./testImage.png')}
+              alt="title"
+              loading="lazy"
+            />
+          </ImageListItem>
+        ))}
+      </ImageList>
     </div>
   );
 }
