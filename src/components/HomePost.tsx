7f86911c4d1e360406f2093e9b445c4d14200335
sa02045 <sa02045@naver.com>
2022-12-17 22:22:29 +0900

    chore: 메인화명 정리

--- src/components/HomePost.tsx
+++ src/components/HomePost.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import Tag from '../components/Tag';
 import './HomePost.css';
 
 interface Props {
@@ -9,28 +8,7 @@ interface Props {
   tagList?: string[];
 }
 function HomePost({ src, zIndex, title }: Props) {
-  const [clicked, setClicked] = React.useState(false);
-  return (
-    <div
-      className={clicked ? 'animateYes post-container' : 'post-container'}
-      style={{ zIndex }}
-      onClick={(e) => {
-        setClicked(!clicked);
-      }}
-    >
-      <img className="main-image" alt="main" src={src} />
-      <section>
-        <div className="title-container">{title}</div>
-        <div className="tag-container">
-          <Tag text="태그태그" />
-          <Tag text="태그태그" />
-          <Tag text="태그태그" />
-          <Tag text="태그태그" />
-          <Tag text="태그태그" />
-        </div>
-      </section>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default HomePost;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,48 +1,9 @@
 import React from 'react';
-import {
-  HeaderBackButton,
-  Header,
-  HeaderSearchButton,
-} from '../components/Headers';
 
-import { useNavigate } from 'react-router-dom';
 import './Home.css';
-import Tag from '../components/Tag';
-import { useSwipeable } from 'react-swipeable';
-import HomePost from '../components/HomePost';
 
 function Home() {
-  const handlers = useSwipeable({
-    onSwiped: (eventData) => console.log('User Swiped!', eventData),
-  });
-  const navigate = useNavigate();
-  return (
-    <div className="home-container">
-      <Header
-        left={<HeaderBackButton />}
-        centerText={'logo'}
-        right={<HeaderSearchButton />}
-      />
-
-      <div className="image-container" {...handlers}>
-        <HomePost
-          title="title1"
-          zIndex={1}
-          src="http://www.kstarfashion.com/news/photo/201812/133429_71560_5413.jpg"
-        />
-        <HomePost
-          title="title2"
-          zIndex={2}
-          src="https://mblogthumb-phinf.pstatic.net/MjAxNzAxMThfNSAg/MDAxNDg0NzA3NDI2NTM0.xPysRQo4Uc-SLxEJ2Se1g7NLeqqkwIvo4zG8ab7eqA8g.hwugr9361_FkOYIWmxzAvb9BnIcGHJfoPPN8sGQItn0g.JPEG.senti54/photo_6.jpg?type=w2"
-        />
-        <HomePost
-          title="title3"
-          zIndex={3}
-          src="http://gdimg.gmarket.co.kr/1689551911/still/600?ver=1571370973"
-        />
-      </div>
-    </div>
-  );
+  return <div className="home-container"></div>;
 }
 
 export default Home;
